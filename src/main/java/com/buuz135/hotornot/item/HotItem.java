765d314be109947455e6170d4e30336246661d05
ACGaming <4818419+ACGaming@users.noreply.github.com>
2021-12-25 15:34:12 +0100

    Fix sneaking using the protection item, closes #1

--- src/main/java/com/buuz135/hotornot/item/HotItem.java
+++ src/main/java/com/buuz135/hotornot/item/HotItem.java
@@ -4,9 +4,12 @@ import java.util.List;
 import javax.annotation.Nullable;
 
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 import com.buuz135.hotornot.HotOrNot;
@@ -33,6 +36,12 @@ public class HotItem extends Item
     {
         super.addInformation(stack, worldIn, tooltip, flagIn);
         tooltip.add(new TextComponentTranslation("item.hotornot.hot_item.tooltip").getUnformattedComponentText());
+    }
+
+    @Override
+    public boolean doesSneakBypassUse(ItemStack stack, IBlockAccess world, BlockPos pos, EntityPlayer player)
+    {
+        return true;
     }
 
     @Override
